4fa04ea3fb9a74a080d0b1bbf61e8bec6d9bc003
Youta <0x59.xxx@gmail.com>
2025-05-28 23:45:26 +0900

    Fix: logo replace left to center

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-black text-white px-6 py-4 flex items-center justify-between shadow-md">
+    <header className="bg-black text-white px-6 py-4 flex justify-center items-center justify-between shadow-md">
       <div className="flex items-center">
         <Image src="/logo.svg" alt="GYMba+R Logo" width={150} height={40} />
       </div>
