81b8850f21fdba186080f1db457032221df86c94
hiromuota166 <otahiromu166@gmail.com>
2025-05-31 09:29:11 +0900

    add 気分的にダブルクオーテーション

--- src/feature/StartTrainingButton/StartTrainingButton.tsx
+++ src/feature/StartTrainingButton/StartTrainingButton.tsx
@@ -1,9 +1,9 @@
-'use client';
-import React from 'react'
-import { useStartTraining } from './useStartTraining'
+"use client";
+import React from "react"
+import { useStartTraining } from "./useStartTraining";
 
 const StartTrainingButton = () => {
-  const { handleClick } = useStartTraining('/training');
+  const { handleClick } = useStartTraining("/training");
   return (
     <div>
       <button onClick={handleClick}>
