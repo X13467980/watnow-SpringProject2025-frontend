76dbe33e4e13d525fbbaa4b4c89ab3212b87f982
Youta <0x59.xxx@gmail.com>
2025-06-10 11:03:44 +0900

    Feat: create Footer component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,7 @@ import Header from '@/feature/Header/Header';
 import { StartTrainingButton } from '@/feature/StartTrainingButton/StartTrainingButton';
 import React from 'react';
 import Link from 'next/link';
+import Footer from '@/feature/Footer/Footer';
 
 export default function HomePage() {
   return (
@@ -15,21 +16,7 @@ export default function HomePage() {
       </main>
 
       <StartTrainingButton />
-      {/* フッター */}
-      <footer className="h-16 bg-black border-t border-gray-700 flex justify-around items-center">
-        {[
-          { label: 'CAMERA', icon: 'camera.svg', link: '/camera' },
-          { label: 'HOME', icon: 'home.svg', link: '/home' },
-          { label: 'LIST', icon: 'menu.svg', link: '/hoge' },
-        ].map(({ label, icon, link }) => (
-          <Link href={link} key={label} className="flex flex-col items-center text-sm text-white">
-          <button className="flex flex-col items-center text-sm text-white">
-            <img src={icon} alt={label} className="w-6 h-6 mb-1" />
-            {label}
-          </button>
-          </Link>
-        ))}
-      </footer>
+      <Footer />
     </div>
   );
 }
