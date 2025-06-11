814da89643d7c480cb7959f3e891ecfd880c4487
hiromuota166 <otahiromu166@gmail.com>
2025-06-11 19:24:01 +0900

    fix tailwind.config.tsを使わず/homeのcss実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,22 @@ import logo from '../../public/logo.svg';
 export default function WelcomePage() {
   return (
     <>
-      <div className="bg-black p-8 text-center min-h-screen flex flex-col justify-center items-center gap-6">
+      <div className="bg-black text-center min-h-screen flex flex-col justify-start items-center gap-6 pt-48">
         <Image
           src={logo}
           alt="Gymba+R Logo"
-          className="w-64 h-64 object-contain rounded-2xl shadow-lg bg-black"
+          className="w-64 h-64 object-contain shadow-lg bg-black"
         />
 
-        <div className="mt-8 flex gap-4">
-          <Link href="/signup">
-            <button className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition">
-              新規登録
+        <div className="grid gap-4 w-[min(70vw,600px)]">
+          <Link href="/login">
+            <button className="bg-none border border-white text-white px-6 py-2 rounded-3xl hover:bg-gray-700 transition text-2xl w-full font-bold">
+              Log In
             </button>
           </Link>
-          <Link href="/login">
-            <button className="bg-gray-600 text-white px-6 py-2 rounded hover:bg-gray-700 transition">
-              ログイン
+          <Link href="/signup">
+            <button className="bg-white text-ritsred px-6 py-2 rounded-3xl hover:bg-blue-700 transition text-2xl w-full font-bold text-red-800">
+              Sign Up
             </button>
           </Link>
         </div>
