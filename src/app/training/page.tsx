b3bed0aa3ff7827e67534edbcc704413a2854081
punksguitar <hanahanaogutaro@gmail.com>
2025-06-28 00:08:01 +0900

    FIX:余計な空白削除

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,11 +1,3 @@
-
 import { MuscleGroupsAccordion } from '@/feature/Training/MuscleGroupsAccordion';
 import React from 'react';
-
-
-  return (
-
-    <MuscleGroupsAccordion />
-  )
-
-
+return <MuscleGroupsAccordion />;
