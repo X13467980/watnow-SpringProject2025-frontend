7346851f4d20b1931d75c12d81c7df580b661bdd
Youta <0x59.xxx@gmail.com>
2025-06-27 18:57:52 +0900

    Feat: add training page with header, accordion, and footer components

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,9 +1,15 @@
 import { MuscleGroupsAccordion } from '@/feature/Training/MuscleGroupsAccordion';
+import Footer from '@/feature/Footer/Footer';
+import Header from '@/feature/Header/Header';
 import React from 'react';
 
 const page = () => {
   return (
-    <MuscleGroupsAccordion />
+    <>
+      <Header />
+      <MuscleGroupsAccordion />
+      <Footer />
+    </>
   )
 }
 
