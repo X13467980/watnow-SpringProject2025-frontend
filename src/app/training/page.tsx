c1b4702fbd40db4968ec9a7ce612061dfbdd2844
Youta <0x59.xxx@gmail.com>
2025-06-27 19:00:20 +0900

    Feat: fix header and footer on training page using fixed positioning

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { MuscleGroupsAccordion } from '@/feature/Training/MuscleGroupsAccordion';
 import Footer from '@/feature/Footer/Footer';
 import Header from '@/feature/Header/Header';
@@ -5,12 +6,18 @@ import React from 'react';
 
 const page = () => {
   return (
-    <>
-      <Header />
-      <MuscleGroupsAccordion />
-      <Footer />
-    </>
-  )
-}
+    <div className="relative min-h-screen">
+      <div className="fixed top-0 left-0 right-0 z-50">
+        <Header />
+      </div>
+      <div className="pt-20 pb-20">
+        <MuscleGroupsAccordion />
+      </div>
+      <div className="fixed bottom-0 left-0 right-0 z-50">
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
